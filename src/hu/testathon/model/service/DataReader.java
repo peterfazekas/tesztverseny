da16934706c9bed1244c02e046e915e64cc86c3d
Peter_Fazekas <v-pfazekas@hotels.com>
2019-03-15 16:13:59 +0100

    DataReader update

--- src/hu/testathon/model/service/DataReader.java
+++ src/hu/testathon/model/service/DataReader.java
@@ -1,7 +1,6 @@
 package hu.testathon.model.service;
 
 import java.io.BufferedReader;
-import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
